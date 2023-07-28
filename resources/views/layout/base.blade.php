b1ed9d1dee1ddb59f9ebfe1f9d81f933702c274a
litos-ua <andrzejlitkiewicz@gmail.com>
2023-07-28 14:37:37 +0200

    modify base.blade.php for examle

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -115,17 +115,7 @@
         @show
                     @section('languageSelector')
                     <!-- Language Selector Dropdown -->
-{{--                    <div class="dropdown language-selector">--}}
-{{--                        <button class="btn btn-secondary dropdown-toggle" type="button" id="languageDropdown" data-bs-toggle="dropdown" aria-expanded="false">--}}
-{{--                            {{__('messages.lang_but')}}--}}
-{{--                        </button>--}}
-{{--                        <img class="flag-icon" src="{{ asset('/pictures/local/flags/english-flag.png') }}" alt="Flag" data-language="en">--}}
-{{--                        <ul class="dropdown-menu" aria-labelledby="languageDropdown">--}}
-{{--                            <li><a class="dropdown-item" href="{{ route(Route::currentRouteName()) }}" data-language="en">English</a></li>--}}
-{{--                            <li><a class="dropdown-item" href="{{ route(Route::currentRouteName()) }}" data-language="pl">Polish</a></li>--}}
-{{--                            <li><a class="dropdown-item" href="{{ route(Route::currentRouteName()) }}" data-language="uk">Ukrainian</a></li>--}}
-{{--                        </ul>--}}
-{{--                    </div>--}}
+
                     @show
         @section('contentHeader2')
                     <button class="navbar-toggler"
