44ce04ce522956a9a578f52255b4707d8ff576cf
litos-ua <andrzejlitkiewicz@gmail.com>
2023-07-30 14:40:42 +0200

    Footer down

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,12 @@
-<footer class="text-muted py-5" id="footer">
+<footer class="text-muted fixed-down  py-5" id="footer">
     <div class="container" id="divfooter">
         <p class="float-end mb-1">
             <a href="#">{{__('messages.to_top')}}</a>
         </p>
         <p class="mb-1">Album example is &copy; Bootstrap,  &copy; PHP,  &copy; Laravel</p>
-        <p class="mb-0">New to Bootstrap?
+        <p class="mb-0">WALENTYNA
             <a href="/">Visit the homepage</a> or read our
-            <a href="https://polsat.pl">getting started guide</a>.</p>
+            <a href="https://polsat.pl">getting started with POLSAT</a>
+        </p>
     </div>
 </footer>
