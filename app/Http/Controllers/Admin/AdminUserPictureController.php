66d475d53ff2bbfba7f34be77e69a1f9259cc6c8
litos-ua <andrzejlitkiewicz@gmail.com>
2023-08-03 16:26:53 +0200

    The input data to database works without duplicate!

--- app/Http/Controllers/Admin/AdminUserPictureController.php
+++ app/Http/Controllers/Admin/AdminUserPictureController.php
@@ -32,7 +32,7 @@ class AdminUserPictureController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'filename' => 'required|string|max:60',
+            'filename' => 'required|string|max:60|unique:pictures',
             'title' => 'required|string|max:60',
             'description' => 'required|string|max:150',
             'category' => 'required|integer',
@@ -42,14 +42,16 @@ class AdminUserPictureController extends Controller
             // If validation passes, insert the data into the database
             Picture::create($request->all());
             session()->flash('success', 'Picture added successfully!');
-            return redirect()->back()->with('success', 'Picture added successfully!');
+            //return redirect()->back()->with('success', 'Picture added successfully!');
+            return response()->json(['message' => 'Picture added successfully']);
         } catch (QueryException $exception) {
             // Catch the QueryException and handle the foreign key constraint violation
             if ($exception->getCode() == 23000) {
                 // If the exception code is 23000 (Integrity constraint violation)
                 // and the error is related to the foreign key constraint (e.g., category value does not exist)
                 // Redirect back with an error message
-                return redirect()->back()->withErrors(['category' => 'Category value does not exist. Please enter a valid value for the category!'])->withInput();
+                //return redirect()->back()->withErrors(['category' => 'Category value does not exist. Please enter a valid value for the category!'])->withInput();
+                return response()->json(['error' => 'Category value does not exist. Please enter a valid value for the category!'], 422);
             }
 
             // If it's not a foreign key constraint violation, rethrow the exception
